1048b9228e54abf0531d9803beea3386d695f658
omar guillen perez <omarnapoleon.ucv@gmail.com>
2023-09-19 13:43:33 -0300

    Add openAPI swagger

--- src/main/java/com/demo/ecommerce/controller/DemoController.java
+++ src/main/java/com/demo/ecommerce/controller/DemoController.java
@@ -1,7 +1,13 @@
 package com.demo.ecommerce.controller;
 
 import com.demo.ecommerce.dto.EcommerceResponseDto;
+import com.demo.ecommerce.dto.ExceptionResponseDto;
 import com.demo.ecommerce.service.EcommerceServiceI;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.constraints.Min;
 import lombok.RequiredArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
@@ -17,6 +23,21 @@ public class DemoController {
 
     private final EcommerceServiceI ecommerceServiceI;
 
+    @Operation(summary = "Find the price of a product on a given date")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Found the price product",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = EcommerceResponseDto.class)) }),
+            @ApiResponse(responseCode = "400", description = "Invalid parameters",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ExceptionResponseDto.class)) }),
+            @ApiResponse(responseCode = "404", description = "Product not found",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ExceptionResponseDto.class)) }),
+            @ApiResponse(responseCode = "500", description = "Internal server error",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ExceptionResponseDto.class)) }
+            ) })
     @GetMapping("/product/price")
     public ResponseEntity<EcommerceResponseDto> getPricing(
             @RequestParam(name = "date")
